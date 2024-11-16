9596172148f28f27650c48c05793f84798250498
FarqadAhmed <frqdalhello@gmail.com>
2024-11-16 12:35:57 +0300

    Parse the JSON response

--- Ass2.php
+++ Ass2.php
@@ -1,5 +1,9 @@
 <?php
+//  link to fetch data from the Bahrain Open Data Portal API
 $URL = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-studentsnationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%2
 0the_programs%20like%20%22bachelor%22&limit=100";
+//  Parse the JSON response
+$response = file_get_contents($URL);
+$result = json_decode($response, true);
 
 ?>
