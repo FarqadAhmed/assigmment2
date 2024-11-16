f8413b1eb2476e297fc1a65f8f4db769e114493d
FarqadAhmed <frqdalhello@gmail.com>
2024-11-16 12:18:26 +0300

    create php file with url

---
+++ Assignmemt2.php
@@ -0,0 +1,6 @@
+<?php
+
+ $URL = h"ttps://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-studentsnationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%2
+0the_programs%20like%20%22bachelor%22&limit=100";
+
+?>
