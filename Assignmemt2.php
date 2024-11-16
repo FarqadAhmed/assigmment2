e1933585fa19dfd94df79c5fc8abd74012c0d66d
FarqadAhmed <frqdalhello@gmail.com>
2024-11-16 12:26:26 +0300

    Delete Assignmemt2.php

--- Assignmemt2.php
+++
@@ -1,6 +0,0 @@
-<?php
-
- $URL = h"ttps://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-studentsnationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%2
-0the_programs%20like%20%22bachelor%22&limit=100";
-
-?>
